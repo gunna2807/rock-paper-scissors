9b080a69a57af2ce5c7542c221da5776ca5e9e6d
Greg Fisher <gunna2807@gmail.com>
2023-07-31 13:17:45 +1000

    Start adding UI to original project.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-This is a first exercise in writing aprogram using javascript. It will be 
-simple and use only the basics neccessary at this stage and will be further 
+This is a first exercise in writing a program using javascript. It will be 
+simple and use only the basics necessary at this stage and will be further 
 developed at a later stage.
 
 Note that if you download the code and want to run it locally you will have 
-to change the start of the links to the other directories as they are
-currently set up to run remotely from github.
+to change the start of the links as they arecurrently set up to run remotely
+from github.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,73 @@
-// rock paper scissors game - a first program in javascript
+// initialise scores etc
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
+// function to generate the computer selection
+function computerPlay() {
+    randomSelection = Math.floor( Math.random() * 3);
+
+    if (randomSelection == '0') {
+        return "ROCK";
+    }
+    else if (randomSelection == '1') {
+        return 'PAPER';
+    }
+    else {
+        return 'SCISSORS';
+    }
+}
+
+// get player selection
+
+
+
+
+// function to play a round
+function playRound( playerSelection, computerSelection) {
+    
+    //get selections
+        computerSelection = computerPlay();
+
+    if (playerSelection === computerSelection) {
+        roundWinner = 'It was a tie!'
+    }
+
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+        playerScore++
+        roundWinner = 'Congratulations you won!'
+    }
+
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ) {
+        computerScore++
+        roundWinner = 'Sorry the computer won!'
+    }  
+
+    return {roundWinner, 
+            playerScore, 
+            computerScore}
+    
+    
+}
+
+
+
+
+
+if ( playerScore > computerScore) {
+    console.log('Congratulations you beat the computer ' + playerScore + ':' + computerScore);
+}
+else if (computerScore > playerScore) {
+    console.log('Sorry the computer won ' + computerScore + ':' + playerScore);
+}
+else {
+    console.log('It was a draw ' + playerScore + ':' + computerScore);
+}
